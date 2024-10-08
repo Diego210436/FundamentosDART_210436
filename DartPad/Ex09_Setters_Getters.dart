1c8a5969e13db6da373341df480f65d58991951c
2104 <210436@utxicotepec.edu.mx>
2024-10-08 08:18:24 -0600

    Practica 12
    Se actualizo el archiivo de manera correcta

--- DartPad/Ex09_Setters_Getters.dart
+++ DartPad/Ex09_Setters_Getters.dart
@@ -1,43 +1,40 @@
 void main() {
-  final mySquare = Square(side: 18); // Instancia de Square con un lado de 18.
+    final mySquare = Square(side: 18);
 
-  // Mostrar el lado y el área usando la propiedad pública y el método           correspondiente.
-  print("""
-  Lado del Cuadrado: ${mySquare.side}
-  Área que ocupa: ${mySquare.calculateArea()}""");
-  
-  mySquare.side = -32; // Esto lanzará una excepción.
-  
+    print("""
+          Lado del Cuadrado: ${mySquare.side}
+          Área que ocupa: ${mySquare.calculateArea()}""");
+
+    mySquare.side = -32;
+
 }
 class Square {
-  double _side; // Variable privada para almacenar el valor del lado del cuadrado.
+    double _side;
 
-  // Constructor que inicializa el lado del cuadrado. El parámetro 'side' es requerido.
-  Square({required double side})
-      : _side = side; // Inicializa la variable privada con el valor del parámetro.
 
-  // Getter para acceder al área (lado * lado).
-  double get area {
-    return _side * _side; // Retorna el área del cuadrado (lado al cuadrado).
-  }
+    Square({required double side})
+    : _side = side;
 
-  // Getter para acceder al valor del lado de manera segura.
-  double get side {
-    return _side; // Retorna el valor del lado.
-  }
 
-  // Setter para asignar un nuevo valor al lado, con validación.
-  set side(double value) {
-    print("Asignando un nuevo valor al lado: $value");
+    double get area {
+        return _side * _side;
+    }
 
-    // Validación: no se permite asignar valores negativos.
-    if (value < 0) throw 'El valor del lado debe ser mayor a 0.';
+    double get side {
+        return _side;
+    }
 
-    _side = value; // Asigna el nuevo valor si es válido.
-  }
 
-  // Método para calcular el área usando el valor del lado.
-  double calculateArea() {
-    return _side * _side; // Devuelve el área calculada.
-  }
+    set side(double value) {
+        print("Asignando un nuevo valor al lado: $value");
+
+        if (value < 0) throw 'El valor del lado debe ser mayor a 0.';
+
+        _side = value;
+    }
+
+
+    double calculateArea() {
+        return _side * _side;
+    }
 }
